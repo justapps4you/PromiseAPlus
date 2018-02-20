479656264bc3f6ffaa4fe860382a53b3898e6910
manueljuan <manueljuan@fb.com>
2018-02-20 12:56:07 +0000

    fixing package error and some cosmetics

--- PromiseAPlus.js
+++ PromiseAPlus.js
@@ -33,9 +33,9 @@ function runPromise(promise, value, state) {
     // 2.3.2.1 If x is pending, promise must remain pending until x is fulfilled or rejected
     if (promise.currentSate === STATE.PENDING) return;
     var callbacks = (promise.currentSate === STATE.FULFILLED) ? promise.fulfillCallbacks : promise.rejectCallbacks;
-    setTimeout(// 2.2.4 onFulfilled or onRejected must not be called until the execution context stack contains only platform code. [3.1]
-        function() {// 2.2.5 onFulfilled and onRejected must be called as functions (i.e. with no this value). [3.2]
-            callbacks.forEach(// 2.2.6 then may be called multiple times on the same promise
+    setTimeout( // 2.2.4 onFulfilled or onRejected must not be called until the execution context stack contains only platform code. [3.1]
+        function() { // 2.2.5 onFulfilled and onRejected must be called as functions (i.e. with no this value). [3.2]
+            callbacks.forEach( // 2.2.6 then may be called multiple times on the same promise
                 function(cb){
                     // 2.2.2.1 (onFulfilled) it must be called after promise is fulfilled, with promise’s value as its first argument.
                     // 2.2.3.1 (onRejected) it must be called after promise is fulfilled, with promise’s value as its first argument.
@@ -46,11 +46,9 @@ function runPromise(promise, value, state) {
     );
     // 2.2.2.3 (onFulfilled) it must not be called more than once
     // 2.2.3.3 (onRejected) it must not be called more than once
-    // 2.3.3.3.3 If both resolvePromise and rejectPromise are called, or multiple calls to the same argument are made, the first call takes precedence, and any further calls are ignored.
     promise.fulfillCallbacks = [];
     promise.rejectCallbacks = [];
 }
-Promise.prototype.catch = function (reject) { return this.then(undefined, reject) };
 function CallBack(promise, callback, state) {
     return function (value) {
         // 2.2.1 Both onFulfilled and onRejected are optional arguments (see 2.2.1 NOTE)
@@ -69,7 +67,7 @@ function CallBack(promise, callback, state) {
             runPromise(promise, e, STATE.REJECTED);
         }
         if (x === promise) runPromise(promise, new TypeError('same object'), STATE.REJECTED);// 2.3.1 If promise and x refer to the same object, reject promise with a TypeError as the reason
-        if (x instanceof Promise) x.then(// 2.3.2 If x is a promise, adopt its state [3.4]
+        if (x instanceof Promise) x.then( // 2.3.2 If x is a promise, adopt its state [3.4]
             function(value){ runPromise(promise, value, STATE.FULFILLED)}, // 2.3.2.2 If/when x is fulfilled, fulfill promise with the same value
             function(reason){ runPromise(promise, reason, STATE.REJECTED)} // 2.3.2.3 If/when x is rejected, reject promise with the same reason
         );
@@ -84,41 +82,43 @@ function CallBack(promise, callback, state) {
                     runPromise(promise, e, STATE.REJECTED);
                     return;
                 }
-                if (typeof then === 'function') {// 2.3.3.3 If then is a function, call it with x as this, first argument resolvePromise, and second argument rejectPromise
-                    var called = false;
+                if (typeof then === 'function') { // 2.3.3.3 If then is a function, call it with x as this, first argument resolvePromise, and second argument rejectPromise
+                    var called = false; // for 2.3.3.3.3 and 2.3.3.3.4.1
                     try {
-                        then.call(x, function(y) {// 2.3.3.3.1 If/when resolvePromise is called with a value y, run [[Resolve]](promise, y)
-                            if (!called) {
+                        then.call(x, function(y) { // 2.3.3.3.1 If/when resolvePromise is called with a value y, run [[Resolve]](promise, y)
+                            if (!called) { // 2.3.3.3.3 If both resolvePromise and rejectPromise are called, or multiple calls to the same argument are made, the first call takes precedence, and any further calls are ignored.
                                 runThePromiseResolutionProcedure(y);
-                                called = true;
+                                called = true; // for for 2.3.3.3.3 and 2.3.3.3.4.1
                             }
-                        },function(r) {// 2.3.3.3.2 If/when rejectPromise is called with a reason r, reject promise with r
-                            if (!called) {
+                        },function(r) { // 2.3.3.3.2 If/when rejectPromise is called with a reason r, reject promise with r
+                            if (!called) { // 2.3.3.3.3 If both resolvePromise and rejectPromise are called, or multiple calls to the same argument are made, the first call takes precedence, and any further calls are ignored.
                                 runPromise(promise, r, STATE.REJECTED);
-                                called = true;
+                                called = true; // for for 2.3.3.3.3 and 2.3.3.3.4.1
                             }
                         })
-                    } catch (e) {// 2.3.3.3.4 If calling then throws an exception e
-                        if(!called) {// 2.3.3.3.4.1 If resolvePromise or rejectPromise have been called, ignore it
+                    } catch (e) { // 2.3.3.3.4 If calling then throws an exception e
+                        if(!called) { // 2.3.3.3.4.1 If resolvePromise or rejectPromise have been called, ignore it
                             runPromise(promise, e, STATE.REJECTED);// 2.3.3.3.4.2 Otherwise, reject promise with e as the reason
                         }
                     }
-                } else {// 2.3.3.4 If then is not a function, fulfill promise with x
+                } else { // 2.3.3.4 If then is not a function, fulfill promise with x
                     runPromise(promise, x, STATE.FULFILLED);
                 }
-            } else {// 2.3.4 If x is not an object or function, fulfill promise with x
+            } else { // 2.3.4 If x is not an object or function, fulfill promise with x
                 runPromise(promise, x, STATE.FULFILLED);
             }
         })(x);
     }
 }
-Promise.reject = function (reason) { // OPTIONAL
-    var promise = new Promise(function () {});
-    runPromise(promise, reason, STATE.REJECTED);
-    return promise;
-};
-Promise.resolve = function (data) { // OPTIONAL
-    if (data instanceof Promise) return data;
-    return new Promise(function(resolve) { resolve(data)});
-};
+/* OPTIONAL */
+// Promise.prototype.catch = function (reject) { return this.then(undefined, reject) };
+// Promise.reject = function (reason) {
+//     var promise = new Promise(function () {});
+//     runPromise(promise, reason, STATE.REJECTED);
+//     return promise;
+// };
+// Promise.resolve = function (data) {
+//     if (data instanceof Promise) return data;
+//     return new Promise(function(resolve) { resolve(data)});
+// };
 module.exports = Promise;
