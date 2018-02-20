cad637d1c573ba464d1fee34bab67c2a6b20a4fb
manueljuan <manueljuan@fb.com>
2018-02-20 14:44:16 +0000

    changing resolvePromise by resolveOrReject

--- PromiseAPlus.js
+++ PromiseAPlus.js
@@ -7,8 +7,8 @@ function Promise(executor) {
     if (typeof executor !== 'function') return Promise.reject('Executor is not a Function');
     var promise = this;
     executor(
-        function(value) { runPromise(promise, value, STATE.FULFILLED) },
-        function(reason) { runPromise(promise, reason, STATE.REJECTED) }
+        function(value) { resolveOrReject(promise, value, STATE.FULFILLED) },
+        function(reason) { resolveOrReject(promise, reason, STATE.REJECTED) }
     );
     return this;
 }
@@ -18,11 +18,11 @@ Promise.prototype.then = function (onFulfilled, onRejected) {
     this.fulfillCallbacks.push(CallBack(promise, onFulfilled, STATE.FULFILLED));
     // 2.2.6.2 If/when promise is rejected, all respective onRejected callbacks must execute in the order of their originating calls to then
     this.rejectCallbacks.push(CallBack(promise, onRejected, STATE.REJECTED));
-    runPromise(this);
+    resolveOrReject(this);
     // 2.2.7 then must return a promise [3.3].
     return promise;
 };
-function runPromise(promise, value, state) {
+function resolveOrReject(promise, value, state) {
     if(state !== undefined) {
         if (promise.currentSate !== STATE.PENDING) return;
         promise.value = value;
@@ -57,19 +57,19 @@ function CallBack(promise, callback, state) {
         if (typeof callback !== 'function'){
             // 2.2.7.3 If onFulfilled is not a function and promise1 is fulfilled, promise2 must be fulfilled with the same value as promise1
             // 2.2.7.4 If onRejected is not a function and promise1 is rejected, promise2 must be rejected with the same reason as promise1
-            runPromise(promise, value, state); // 2.2.1 NOTE: ignored by recursive call with empty fulfillCallbacks & rejectCallbacks lists
+            resolveOrReject(promise, value, state); // 2.2.1 NOTE: ignored by recursive call with empty fulfillCallbacks & rejectCallbacks lists
         }
         var x;
         try {
             x = callback(value);
         } catch (e) {
             // 2.2.7.2 If either onFulfilled or onRejected throws an exception e, promise2 must be rejected with e as the reason
-            runPromise(promise, e, STATE.REJECTED);
+            resolveOrReject(promise, e, STATE.REJECTED);
         }
-        if (x === promise) runPromise(promise, new TypeError('same object'), STATE.REJECTED);// 2.3.1 If promise and x refer to the same object, reject promise with a TypeError as the reason
+        if (x === promise) resolveOrReject(promise, new TypeError('same object'), STATE.REJECTED);// 2.3.1 If promise and x refer to the same object, reject promise with a TypeError as the reason
         if (x instanceof Promise) x.then( // 2.3.2 If x is a promise, adopt its state [3.4]
-            function(value){ runPromise(promise, value, STATE.FULFILLED)}, // 2.3.2.2 If/when x is fulfilled, fulfill promise with the same value
-            function(reason){ runPromise(promise, reason, STATE.REJECTED)} // 2.3.2.3 If/when x is rejected, reject promise with the same reason
+            function(value){ resolveOrReject(promise, value, STATE.FULFILLED)}, // 2.3.2.2 If/when x is fulfilled, fulfill promise with the same value
+            function(reason){ resolveOrReject(promise, reason, STATE.REJECTED)} // 2.3.2.3 If/when x is rejected, reject promise with the same reason
         );
         // 2.2.7.1 If either onFulfilled or onRejected returns a value x, run the Promise Resolution Procedure [[Resolve]](promise2, x)
         (function runThePromiseResolutionProcedure(x) {
@@ -79,7 +79,7 @@ function CallBack(promise, callback, state) {
                 try {
                     then = x.then; // 2.3.3.1 Let then be x.then. [3.5]
                 } catch (e) { // 2.3.3.2 if retrieving the property x.then results in a thrown exception e, reject promise with e as the reason.
-                    runPromise(promise, e, STATE.REJECTED);
+                    resolveOrReject(promise, e, STATE.REJECTED);
                     return;
                 }
                 if (typeof then === 'function') { // 2.3.3.3 If then is a function, call it with x as this, first argument resolvePromise, and second argument rejectPromise
@@ -92,20 +92,20 @@ function CallBack(promise, callback, state) {
                             }
                         },function(r) { // 2.3.3.3.2 If/when rejectPromise is called with a reason r, reject promise with r
                             if (!called) { // 2.3.3.3.3 If both resolvePromise and rejectPromise are called, or multiple calls to the same argument are made, the first call takes precedence, and any further calls are ignored.
-                                runPromise(promise, r, STATE.REJECTED);
+                                resolveOrReject(promise, r, STATE.REJECTED);
                                 called = true; // for for 2.3.3.3.3 and 2.3.3.3.4.1
                             }
                         })
                     } catch (e) { // 2.3.3.3.4 If calling then throws an exception e
                         if(!called) { // 2.3.3.3.4.1 If resolvePromise or rejectPromise have been called, ignore it
-                            runPromise(promise, e, STATE.REJECTED);// 2.3.3.3.4.2 Otherwise, reject promise with e as the reason
+                            resolveOrReject(promise, e, STATE.REJECTED);// 2.3.3.3.4.2 Otherwise, reject promise with e as the reason
                         }
                     }
                 } else { // 2.3.3.4 If then is not a function, fulfill promise with x
-                    runPromise(promise, x, STATE.FULFILLED);
+                    resolveOrReject(promise, x, STATE.FULFILLED);
                 }
             } else { // 2.3.4 If x is not an object or function, fulfill promise with x
-                runPromise(promise, x, STATE.FULFILLED);
+                resolveOrReject(promise, x, STATE.FULFILLED);
             }
         })(x);
     }
@@ -114,7 +114,7 @@ function CallBack(promise, callback, state) {
 // Promise.prototype.catch = function (reject) { return this.then(undefined, reject) };
 // Promise.reject = function (reason) {
 //     var promise = new Promise(function () {});
-//     runPromise(promise, reason, STATE.REJECTED);
+//     resolveOrReject(promise, reason, STATE.REJECTED);
 //     return promise;
 // };
 // Promise.resolve = function (data) {
